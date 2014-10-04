8f1fc96eb1b0c598ae18e8623ad341fba81f3f23
Jeremy Schulz <j.schulz@jacobs-university.de>
2014-10-04 11:56:36 +0200

    player class

---
+++ classes.py
@@ -0,0 +1,10 @@
+"""Class Decleration"""
+
+class player:
+	def __init__(self,name):
+		self.name=name
+		self.health=100
+		self.hitchance=0.3
+		self.inventory=[]
+	def take_item(self,item):
+		self.inveto
